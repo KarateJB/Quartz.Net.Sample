f8c3c107b2215eea99dab0ecf0936fcf85d07bb0
JB Lin <JB.Lin@xuenn.com>
2023-07-15 01:06:58 +0800

    Init

---
+++ src/Spectrel.Console.Sample/Program.cs
@@ -0,0 +1,8 @@
+﻿using Spectre.Console;
+using System.Threading.Tasks;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string IPAddress = AnsiConsole.Prompt(new TextPrompt<string>("Enter IPAddress to connect :"));
+string Port = AnsiConsole.Prompt(new TextPrompt<string>("Enter Port to connect :"));
