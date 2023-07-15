68f6a103bccde005bf686d124a5561456819014b
karatejb <ppippenkimo@gmail.com>
2023-07-15 23:07:38 +0800

    Update Program.cs

--- src/Quartz.Net.Sample/Program.cs
+++ src/Quartz.Net.Sample/Program.cs
@@ -1,8 +1,15 @@
 ﻿using Spectre.Console;
 using System.Threading.Tasks;
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace Quartz.Net.Sample;
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        if (args.Length > 0 && args[0].Equals("-i"))
+        {
+            // Environment.Se
+        }
+    }
+}
 
-string IPAddress = AnsiConsole.Prompt(new TextPrompt<string>("Enter IPAddress to connect :"));
-string Port = AnsiConsole.Prompt(new TextPrompt<string>("Enter Port to connect :"));
