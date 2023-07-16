5c95c34332548e7b2e751cc686ad7fedfe2bbc16
karatejb <ppippenkimo@gmail.com>
2023-07-16 18:46:42 +0800

    Add dependencies

--- src/Quartz.Net.Sample/Program.cs
+++ src/Quartz.Net.Sample/Program.cs
@@ -1,5 +1,9 @@
-﻿using Spectre.Console;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Quartz.Net.Sample.Services;
+using Quartz.Net.Sample.Utils.Constants;
+using NLog;
 
 namespace Quartz.Net.Sample;
 public class Program
@@ -8,8 +12,35 @@ public class Program
     {
         if (args.Length > 0 && args[0].Equals("-i"))
         {
-            // Environment.Se
+            Environment.SetEnvironmentVariable(EnvConstants.InteractiveMode, "1");
+            var host = (await CreateHostBuilderAsync(args, isInteractive: true)).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var ia = scope.ServiceProvider.GetRequiredService<IInteractiveMode>();
+                if (await ia.PromptAsync())
+                {
+                    Environment.Exit(0);
+                }
+            }
+            // host.Run();
         }
+        else
+        {
+            Environment.SetEnvironmentVariable(EnvConstants.InteractiveMode, "0");
+            (await CreateHostBuilderAsync(args)).Build().Run();
+        }
+    }
+
+    public static async Task<IHostBuilder> CreateHostBuilderAsync(string[] args, bool isInteractive = false)
+    {
+        var hostBuilder = Host.CreateDefaultBuilder(args)
+            .ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+            });
+
+        return await Task.FromResult(hostBuilder);
     }
 }
 
