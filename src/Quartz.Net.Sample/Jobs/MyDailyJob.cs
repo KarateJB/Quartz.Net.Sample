551dcf9a2428597fdeec75e49d813536e430bda7
karatejb <ppippenkimo@gmail.com>
2023-07-20 09:30:47 +0800

    Update MyDailyJob

--- src/Quartz.Net.Sample/Jobs/MyDailyJob.cs
+++ src/Quartz.Net.Sample/Jobs/MyDailyJob.cs
@@ -1,5 +1,9 @@
 using System.ComponentModel;
-using Quartz;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Quartz.Net.Sample.Models.Config;
+using Quartz.Net.Sample.Services;
+using static Quartz.Net.Sample.Utils.Extensions.IServiceCollectionExtensions;
 
 namespace Quartz.Net.Sample.Jobs;
 
@@ -7,8 +11,47 @@ namespace Quartz.Net.Sample.Jobs;
 [Description("My Daily Job")]
 public class MyDailyJob : IJob
 {
+    private readonly string jobClass = string.Empty;
+    private readonly ILogger<MyDailyJob> logger;
+    private readonly AppSetting appSetting;
+    private readonly MyTaskService ts;
+
+    public MyDailyJob(
+            ILogger<MyDailyJob> logger,
+            IOptions<AppSetting> configuration,
+            MyTaskResolver taskResolver)
+    {
+        this.jobClass = nameof(MyDailyJob);
+        this.logger = logger;
+        this.appSetting = configuration.Value;
+        this.ts = taskResolver(nameof(MyTaskService)) as MyTaskService;
+    }
+
     public async Task Execute(IJobExecutionContext context)
     {
+        bool isSuccess = false;
 
+        #region Retry
+        if (context.RefireCount > this.appSetting.Quartz.RetryMaxTimes)
+        {
+            this.logger.LogError($"{jobClass} retried for more than {this.appSetting.Quartz.RetryMaxTimes} but all failed, stop retrying.");
+            return;
+        }
+        #endregion
+
+        try
+        {
+
+        }
+        catch (System.Exception ex)
+        {
+            isSuccess = false;
+            this.logger.LogError($"{jobClass} failed. {ex.Message}");
+            throw new Quartz.JobExecutionException(msg: $"Start retrying {jobClass}", cause: ex, refireImmediately: true);
+        }
+        finally
+        {
+            this.logger.LogInformation($"{jobClass} {(isSuccess ? "succeeded" : "failed")}");
+        }
     }
 }
--- src/Quartz.Net.Sample/Models/Config/AppSetting.cs
+++ src/Quartz.Net.Sample/Models/Config/AppSetting.cs
@@ -2,5 +2,13 @@ namespace Quartz.Net.Sample.Models.Config;
 
 public class AppSetting
 {
+    public QuartzOption Quartz { get; set; }
+}
 
+public class QuartzOption
+{
+    public int RetryMaxTimes { get; set; }
+
+    // Skip strong typing, see "IServiceCollectionQuartzConfiguratorExtensions.AddJobAndTrigger".
+    // public List<JobOption> Job {get;set;} 
 }
